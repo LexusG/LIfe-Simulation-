81d3aa273e9f786b7c496c69da378b76c32ca8d4
LexusG <33048777+LexusG@users.noreply.github.com>
2023-11-17 18:36:34 -0500

    update

---
+++ import pygame.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+YELLOW = (255, 255, 0 )
+
+# Dimension for gird screen
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20 
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+FPS = 60  ghf
